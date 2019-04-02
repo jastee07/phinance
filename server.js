a4139d25aba0aa245b487d8e70c8806006cf823a
Jake <jastee07@louisville.edu>
2019-04-01 21:32:36 -0400

    Added Keys

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
 app.use(passport.initialize());
 
 //Passport Config
-require("./config/passport")(passport);
+//require("./config/passport")(passport);
 
 app.get("/", (req, res) => res.send("Hello"));
 
