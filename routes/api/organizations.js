59729125a28e32b574b0c91926cbdf54cee6889f
Jake <jake.steele@louisville.edu>
2019-04-02 15:37:06 -0400

    Added PUT for budget

--- routes/api/organizations.js
+++ routes/api/organizations.js
@@ -73,7 +73,7 @@ router.post(
         .status(400)
         .json({ error: "User needs admin status to do this" });
     }
-    Organization.findOneById(req.user.organization).then(organization => {
+    Organization.findById(req.user.organization).then(organization => {
       const newBudget = {
         title: req.body.title,
         amount: req.body.amount,
@@ -85,6 +85,68 @@ router.post(
 
       organization.save().then(organization => res.json(organization));
     });
+  }
+);
+
+// @route   PUT api/organization/budget/:id
+// @desc    Add budget to organization
+// @access  Private
+router.put(
+  "/budget/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //const { errors, isValid } = validateExperienceInput(req.body);
+
+    // Check Validation
+    // if (!isValid) {
+    //   // Return any errors with 400 status
+    //   return res.status(400).json(errors);
+    // }
+    if (req.user.role !== "admin") {
+      return res
+        .status(400)
+        .json({ error: "User needs admin status to do this" });
+    }
+
+    Organization.findById(req.user.organization).then(organization => {
+      // Add to budgets array
+      if (!organization) {
+        return res.status(404).json({ error: "Budget does not exist" });
+      }
+      const id = req.params.id;
+      organization.budgets.id(id).title = req.body.title;
+      organization.budgets.id(id).amount = req.body.amount;
+      organization.budgets.id(id).revenue = req.body.revenue;
+
+      organization.save().then(organization => res.json(organization));
+    });
+  }
+);
+
+// @route   DELETE api/organization/budget/:id
+// @desc    Delete budget from organization
+// @access  Private
+router.delete(
+  "/budget/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Organization.findById(req.user.organization)
+      .then(organization => {
+        if (req.user.role !== "admin") {
+          return res
+            .status(400)
+            .json({ unauthorized: "Must be an admin to access this page" });
+        }
+
+        if (!organization.budgets.id(req.params.id)) {
+          return res.status(404).json({ error: "Budget does not exist" });
+        }
+        // Splice out of array
+        organization.budgets.pull({ _id: req.params.id });
+        // Save
+        organization.save().then(organization => res.json(organization));
+      })
+      .catch(err => res.status(404).json(err));
   }
 );
 
@@ -121,29 +183,6 @@ router.post(
 
       organization.save().then(organization => res.json(organization));
     });
-  }
-);
-
-// @route   DELETE api/organization/budget/:bud_title
-// @desc    Delete budget from organization
-// @access  Private
-router.delete(
-  "/budget/:bud_title",
-  //passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Organization.findOne({ name: req.body.name })
-      .then(organization => {
-        const removeIndex = organization.budgets
-          .map(item => item.title)
-          .indexOf(req.params.bud_title);
-
-        // Splice out of array
-        organization.budgets.splice(removeIndex, 1);
-
-        // Save
-        organization.save().then(organization => res.json(organization));
-      })
-      .catch(err => res.status(404).json(err));
   }
 );
 
