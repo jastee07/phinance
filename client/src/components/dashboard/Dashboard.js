0af11c19d04bda4bac2c3b5ed409c8cdeaf65366
Malik Shalash <mjshal03@louisville.edu>
2019-04-06 13:32:00 -0400

    Still in progress, not done with dashboard initial construct at all

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,8 +3,45 @@ import SimpleCard from "../dashboard/SimpleCard";
 import AreaChart from "./Charts/AreaChart";
 import BarChart from "./Charts/BarChart";
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+
+  onDeleteClick(e) {
+    this.props.deleteAccount();
+  }
+
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    if (profile === null || loading) {
+      dashboardContent = <Spinner />;
+    } else {
+      // Check if logged in user has empty profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = (
+          <div>
+            <p className="lead text-muted">
+              Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+            </p>
+            <ProfileActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
+            {/* TODO: exp and edu*/}
+            <div style={{ marginBottom: "60px" }} />
+            <button
+              onClick={this.onDeleteClick.bind(this)}
+              className="btn btn-danger"
+            >
+              Delete My Account
+            </button>
+          </div>
+        );
+      }
+
+
     return (
       <div>
         <div className="row">
@@ -22,3 +59,20 @@ export default class Dashboard extends Component {
     );
   }
 }
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Dashboard);
