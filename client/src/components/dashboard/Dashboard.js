1b2f7374d2bd2d2283c2954d0b8984e102d8f586
Malik Shalash <mjshal03@louisville.edu>
2019-04-06 08:44:39 -0400

    Added theme files in here as well, also created outlines for components we will need
    Charts
    Cards

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import SimpleCard from "../dashboard/SimpleCard";
+import AreaChart from "./Charts/AreaChart";
+import BarChart from "./Charts/BarChart";
 
 export default class Dashboard extends Component {
   render() {
     return (
       <div>
-        <h1>Dashboard</h1>
+        <div className="row">
+          <div className="col-3">
+            <SimpleCard />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xl-8 col-lg-7">
+            <AreaChart />
+            <BarChart />
+          </div>
+        </div>
       </div>
     );
   }
